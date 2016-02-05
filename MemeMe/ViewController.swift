777f92c323bdf701dcea659c7c858bae49916987
sahil garg <sahilgargin@gmail.com>
2016-02-06 04:04:46 +0530

    incrementing label

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -10,16 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var label: UILabel!
+    var count = 1
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //Label
+        var label = UILabel()
+        label.frame = CGRectMake(150, 150, 60, 60)
+        label.text = "0"
+        self.view.addSubview(label)
+        
+        //Button
+        var button  = UIButton()
+        button.frame = CGRectMake(150, 250, 60, 60)
+        button.setTitle("Click", forState: .Normal)
+        button.setTitleColor(UIColor.blueColor(), forState: .Normal)
+        self.view.addSubview(button)
+        
+        //Adding taget action method to button
+        button.addTarget(self, action: "incrementCount", forControlEvents: UIControlEvents.TouchUpInside)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    func incrementCount() {
+       self.count++
+        self.label.text = "\(self.count)"
+        
     }
-
 
 }
 
