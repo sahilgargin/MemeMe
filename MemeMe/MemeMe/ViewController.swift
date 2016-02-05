85133b5d0588916b0480793db9385e0167ca2d94
sahil garg <sahilgargin@gmail.com>
2016-02-06 02:51:43 +0530

    Setup

---
+++ MemeMe/MemeMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MemeMe
+//
+//  Created by Sahil Garg on 06/02/16.
+//  Copyright (c) 2016 sahilgarg.in. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
